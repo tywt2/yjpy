3616305bbe59da5df335341f06c6f50f92b08760
tywt2 <328820683@qq.com>
2019-08-15 11:15:53 +0800

    20190815

--- computeData.py
+++ computeData.py
@@ -1,50 +1,49 @@
+import json
 
 import requests
-
 from distance import getDistance
 
-url_guotu="http://localhost:8080/GuoTu"
-params_guotu ={
+url_guotu = "http://localhost:8080/GuoTu"
+params_guotu = {
     'departmentcode': 'Guott002'
 }
 guotu_data = requests.request("post", url_guotu, params=params_guotu)
-guotu_json=guotu_data.json()
+guotu_json = guotu_data.json()
 
-url_jiaotong="http://localhost:8080/JiaoTong"
-params_jiaotong ={
+url_jiaotong = "http://localhost:8080/JiaoTong"
+params_jiaotong = {
     'departmentcode': 'Jiaott001'
 }
 jiaotong_data = requests.request("post", url_jiaotong, params=params_jiaotong)
-jiaotong_json=jiaotong_data.json()
+jiaotong_json = jiaotong_data.json()
 
-url_dlb="http://localhost:8080/statusInfo"
-params_dlb ={
+url_dlb = "http://localhost:8080/statusInfo"
+params_dlb = {
     'fileName': 'Status_001814080001_20190521120000384.xml'
 }
 dlb_data = requests.request("post", url_dlb, params=params_dlb)
-dlb_json=dlb_data.json()
-print(dlb_json)
+dlb_json = dlb_data.json()
+
 
 def areaGuoTUPoint(dis):
-    num1=0
-    num2=0
+    num1 = 0
+    num2 = 0
     for guotu in guotu_json:
         Lat1 = guotu['t11']
         Lon1 = guotu['t10']
         Lat1 = float(Lat1)
         Lon1 = float(Lon1)
-        #distance = geopy.distance.geodesic((Lat0, Lon0), (Lat1, Lon1)).km
-        distance=getDistance(Lat0, Lon0,Lat1, Lon1)
+        distance = getDistance(Lat0, Lon0, Lat1, Lon1)
         if (distance < dis):
-            #print(str(guotu['id']) + ' ' + str(guotu['t13']) + ' ' + str(guotu['t14']))
-            num1 +=float(guotu['t13'])
-            num2 +=float(guotu['t14'])
+            num1 += float(guotu['t13'])
+            num2 += float(guotu['t14'])
             print(guotu)
-    return str(num1)+' '+str(num2)
+    return 'person:'+str(num1) + ' monney:' + str(num2)
+
 
 def areaJTPoint(dis):
     for jiaotong in jiaotong_json:
-        #起始点经纬度
+        # 起始点经纬度
         Lat1 = jiaotong['t7']
         Lon1 = jiaotong['t6']
         Lat2 = jiaotong['t9']
@@ -53,29 +52,20 @@ def areaJTPoint(dis):
         Lon1 = float(Lon1)
         Lat2 = float(Lat2)
         Lon2 = float(Lon2)
-        #distance = geopy.distance.geodesic((Lat0, Lon0), (Lat1, Lon1)).km
-        distance=getDistance(Lat0, Lon0,Lat1, Lon1)
-        distance2= getDistance(Lat0, Lon0, Lat2, Lon2)
+        distance = getDistance(Lat0, Lon0, Lat1, Lon1)
+        distance2 = getDistance(Lat0, Lon0, Lat2, Lon2)
         if (distance < dis):
-            #print(str(jiaotong['id']) + ' ' + str(jiaotong['t1']))
             print(jiaotong)
         if (distance2 < dis):
-            #print(str(jiaotong['id']) + ' ' + str(jiaotong['t1']))
             print(jiaotong)
-        return
+    return
 
 
-n=0
 for dlb in dlb_json:
     Lat0 = dlb['Lat']
     Lon0 = dlb['Lon']
     Lat0 = float(Lat0)
     Lon0 = float(Lon0)
 
-    print(str(dlb['ClientID'])+' '+areaGuoTUPoint(1))
-    #print(areaJTPoint(10))
-
-# Lat0 = dlb_json[5]['Lat']
-# Lon0 = dlb_json[5]['Lon']
-# Lat0 = float(Lat0)
-# Lon0 = float(Lon0)
+    print(str(dlb['ClientID']) + '-' + areaGuoTUPoint(1))
+    # print(areaJTPoint(50))
---
+++ net.py
@@ -0,0 +1,17 @@
+# -*- coding: cp936 -*-
+import networkx as nx
+import matplotlib.pyplot as plt
+from random import randint
+G=nx.Graph()
+#for i in range(3):
+#G.add_node(i)
+G.add_weighted_edges_from([(0,1,3.0),(1,2,100),(0,2,5.5)])#添加边的权值
+
+pos = [(110.999444444444,35.0216666666667),(110.795277777778,35.2052777777778),(110.613888888889,35.1211111111111)]#元组中的两个数字是第i（从0开始计数）个点的坐标
+
+nx.draw(G,pos,with_labels=True,node_color='b')#按参数构图
+
+plt.xlim(0,180)#设置首界面X轴坐标范围
+plt.ylim(0,90)#设置首界面Y轴坐标范围
+
+plt.show()#显示图像
